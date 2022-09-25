903826bdbec7e7a4d87028efd91e3ea17c8e0095
Sasha <40319171+lonagi@users.noreply.github.com>
2022-09-25 17:55:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-exports.printMsg = function() {
-    console.log("This is a message from paste image package");
+function getPasteImage(paste, callback){
+    if(!paste.clipboardData) {
+        if(typeof(callback) == "function")
+            callback(undefined)
+    }
+
+    let files = paste.clipboardData.items
+
+    if(files === undefined) {
+        if(typeof(callback) == "function")
+            callback(undefined)
+    }
+
+    for (var i = 0; i < files.length; i++) {
+        if (files[i].type.indexOf("image") === -1)
+            continue
+        let blob = files[i].getAsFile()
+
+        if(typeof(callback) == "function")
+            callback(blob)
+    }
+}
+
+module.exports = {
+    getPasteImage: getPasteImage
 }
